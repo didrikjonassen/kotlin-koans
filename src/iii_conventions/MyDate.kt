0f699ec34f0dd53c2cf275efef972b58c9580652
Didrik Jonassen <jonassen.didrik@gmail.com>
2016-02-19 19:45:52 +0100

    iii25

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,15 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) {
+    infix operator fun compareTo(that: MyDate): Int {
+        return if (year != that.year)
+            year.compareTo(that.year)
+        else if (month != that.month)
+            month.compareTo(that.month)
+        else
+            dayOfMonth.compareTo(that.dayOfMonth)
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
