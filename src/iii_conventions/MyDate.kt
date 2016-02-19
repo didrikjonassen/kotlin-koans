2703ef25a4943325bd6abb0372ec56b0349b7f59
Didrik Jonassen <jonassen.didrik@gmail.com>
2016-02-19 19:50:56 +0100

    iii26

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -19,4 +19,8 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val endInclusive: MyDate) {
+    infix operator fun contains(date: MyDate): Boolean {
+        return start < date && date <= endInclusive
+    }
+}
